e79abb4ef4d2ec05dcb36ac2ad985c228b7db801
PrechaChang <117138296+PrechaChang@users.noreply.github.com>
2023-07-27 15:11:44 +0700

    up

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Student
+
+@admin.register(Student)
+class Admin(admin.ModelAdmin):
+    list_display = ("std_id","name","prefix","lastname","phone")
+    search_fields = ("std_id","name","prefix","lastname","phone")
--- webpage/models.py
+++ webpage/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+prefix_choices = (
+    (1,"นาย"),
+    (2,"นางสาว"),
+    (3,"นาง"),
+)
+class Student (models.Model):
+
+    std_id = models.IntegerField()
+    prefix = models.IntegerField(choices=prefix_choices,default=1)
+    name = models.CharField(max_length=255)
+    lastname = models.CharField (max_length=255)
+    phone = models.CharField (max_length=15)
+    address = models.TextField ()
+    
+
+    class Meta:
+        verbose_name = ("Student")
+        verbose_name_plural = ("Students")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reversed("Student _detail", kwargs={"pk": self.pk})
