95c315e0349a37aec7d2c310e3799906a3c18917
PrechaChang <stu6512732106@sskru.ac.th>
2023-08-07 16:24:22 +0700

    major

--- webpage/admin.py
+++ webpage/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Student
+from .models import Student,Major
 
 @admin.register(Student)
 class Admin(admin.ModelAdmin):
-    list_display = ("std_id","prefix","name","lastname","phone")
-    search_fields = ("std_id","prefix","name","lastname","phone")
+    list_display = ("std_id","prefix","name","lastname","phone","major")
+    search_fields = ("std_id","prefix","name","lastname","phone","major")
+
+@admin.register(Major)
+class MajorAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    
+   
--- webpage/models.py
+++ webpage/models.py
@@ -6,6 +6,17 @@ prefix_choices = (
     (2,"นางสาว"),
     (3,"นาง"),
 )
+class Major(models.Model):
+
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Major"
+        verbose_name_plural = "Majors"
+
+    def __str__(self):
+        return self.name
+
 class Student (models.Model):
 
     std_id = models.IntegerField()
@@ -14,7 +25,7 @@ class Student (models.Model):
     lastname = models.CharField (max_length=255)
     phone = models.CharField (max_length=15)
     address = models.TextField ()
-    
+    major = models.ForeignKey(Major, on_delete=models.CASCADE, default=None, null=True , blank=True) 
 
     class Meta:
         verbose_name = ("Student")
